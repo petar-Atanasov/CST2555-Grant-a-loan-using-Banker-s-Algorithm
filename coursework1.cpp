e60d3723a66be8e062216f1df10c63bc40d24bbd
Petar Atanasov <149053952+petar-Atanasov@users.noreply.github.com>
2024-01-05 00:04:06 +0000

    Complete Coursework1.cpp

--- coursework1.cpp
+++ coursework1.cpp
@@ -11,37 +11,36 @@ M00916537
 using namespace std;
 
 // number of processes
-const int n_of_processes = 5;
+                        // wrong procesess: 4-->3
+const int n_of_processes = 5;           
 // number of resources
+                        // wrong resourcess: 3-->5
 const int n_of_resources = 3;
 
 //get the values for allocation for safe state
 int allocation[n_of_processes][n_of_resources] = { 
                         //here will be the values where deadlock occur  
-    {0, 1, 0}, //P0      //3, 2, 0
-    {2, 0, 0}, //P1      //2, 0, 2
-    {3, 0, 2}, //P2      //3, 0, 2
-    {2, 1, 1}, //P3      //5, 1, 1
-    {0, 0, 2}  //P4      //0, 10, 2
+    {0, 1, 0}, //P0      //3, 2, 0      //1, 1, 1       //2, 1, 0     //0, 1, 2, 1, 0
+    {2, 0, 0}, //P1      //2, 0, 2      //2, 2, 2       //3, 0, 2     //1, 2, 1, 0, 0
+    {3, 0, 2}, //P2      //3, 0, 2      //3, 3, 3       //2, 1, 1    //2, 1, 1, 1, 1
+    {2, 1, 1}, //P3      //5, 1, 1      //4, 4, 4       //0, 0, 2
+    {0, 0, 2}  //P4      //0, 10, 2     //5, 5, 5
 };
 
 // get the values for maximum for safe state
 int maximum[n_of_processes][n_of_resources] = {
                         //here will be the values where deadlock occur
-    {7, 5, 3}, //P0      //7, 5, 3      
-    {3, 2, 2}, //P1      //13, 2, 2 
-    {9, 0, 2}, //P2      //9, 0, 2
-    {4, 2, 2}, //P3      //10, 4, 6
-    {5, 3, 3}  //P4      //5, 3, 3
+    {7, 5, 3}, //P0      //7, 5, 3      //6, 6, 6       //7, 5, 3   //2, 2, 2, 2, 2
+    {3, 2, 2}, //P1      //13, 2, 2     //7, 7, 7       //3, 2, 2   //2, 3, 2, 1, 2  
+    {9, 0, 2}, //P2      //9, 0, 2      //8, 8, 8       //9, 0, 2   //3, 2, 2, 9, 2
+    {4, 2, 2}, //P3      //10, 4, 6     //8, 8, 8       //4, 2, 2
+    {5, 3, 3}  //P4      //5, 3, 3      //9, 9, 9
 };
 
 // get the available values for the banker's algorithm
-int current_Available[n_of_resources] = {
-    3,
-    3,
-    2
-    };
-
+                        // wrong inputs: 2, 6, 2, 2, 6
+int current_Available[n_of_resources] = {3, 3, 2};
+// track the processes if they are finished or not 
 bool finish[n_of_processes] = {false};
 
 // create a Need function to get the needed values for the algorithm
@@ -69,7 +68,6 @@ bool Safe() {
 
     // create a counter which will keep up with the function then 
     //initialize a copy of finish and set the finish from above to it and loop throught it    
-    
     int count = 0;
     
     bool copyOfFinish[n_of_processes];
@@ -98,7 +96,7 @@ bool Safe() {
                     }
                 }
                 // step 3 
-                //after the remaining need check if resources are available
+                //after the remaining need, check if resources are available
                 if (exProcess){
                     for (int k = 0; k < n_of_resources; k++){
                         // allocate the resources
@@ -109,13 +107,13 @@ bool Safe() {
                     // the process is finished
                     copyOfFinish[i] = true;
                     // set the safe sequence array with the count increment
-                    //and assing variable i to it
+                    //and assigning variable i to it
                     safeSeq[count++] = i;
                     found = true;
                 }
             }
         }
-        // if nothing is true set it to 0 
+        // if nothing is true set an error conditian 
         if(!found){
             return false;
         }
@@ -137,44 +135,45 @@ bool Safe() {
     cout << " P" << safeSeq[n_of_processes - 1] << endl;
     return true;
 } 
- bool resource_request (int request[][n_of_resources]){
+// resource-request algorithm 
+ bool resource_request(int request[][n_of_resources]){
     int checkNeed[n_of_processes][n_of_resources];
     need(checkNeed);
 
-    // iterate between the procceses 
-    for ( int k = 0; k < n_of_processes; k++){
+    // step 1 --> iterate between the procceses 
+    for ( int i = 0; i < n_of_processes; i++){
         // check the request exceed it's maximum
-        for (int i = 0; i < n_of_resources; i++){
-            if (request[k][i] > checkNeed[k][i]){
+        for (int j = 0; j < n_of_resources; j++){
+            if (request[i][j] > checkNeed[i][j]){
                 cout << "Processes reached its maximum claim" << endl;
                 return false;
             }
         }
     
-     // check if request it is within available resources 
-        for( int f = 0; f < n_of_resources; f++){
-            if(request[k][f] > current_Available[f]){
+     //step 2 --> check if request it is within available resources 
+        for(int j = 0; j < n_of_resources; j++){
+            if(request[i][j] > current_Available[j]){
                 cout << "Resources aren't available" << endl;
                 return false;
             }
         }
     
-        //pretend to allocate request 
-        for ( int y = 0; y < n_of_resources; y++){
-            current_Available[y] -= request[k][y];
-            allocation[k][y] += request[k][y];
-            checkNeed[k][y] -= request[k][y];
+        //step 3 --> pretend to allocate request 
+        for (int j = 0; j < n_of_resources; j++){
+            current_Available[j] -= request[i][j];
+            allocation[i][j] += request[i][j];
+            checkNeed[i][j] -= request[i][j];
         }
 
-        // based on safety algorithm 
+        // step 4 -->  based on safety algorithm 
         if(!Safe()){
             cout << "UNSAFE STATE." << endl;
 
             // restore the resource-allocation state
-            for(int t = 0; t < n_of_resources; t++){
-                current_Available[t] += request[k][t];
-                allocation[k][t] -= request[k][t];
-                checkNeed[k][t] += request[k][t];
+            for(int j = 0; j < n_of_resources; j++){
+                current_Available[j] += request[i][j];
+                allocation[i][j] -= request[i][j];
+                checkNeed[i][j] += request[i][j];
             }
             return false;
         }
